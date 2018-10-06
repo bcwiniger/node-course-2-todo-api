b63055038a414326bb43423fee5c21cfbb85649c
Beau Winiger <bcwiniger@gmail.com>
2018-10-06 15:16:26 -0400

    fix typo

--- server/server.js
+++ server/server.js
@@ -51,8 +51,8 @@ app.get('/todos/:id', (req, res) => {
         });
 });
 
-app.listen(PORT, () => {
-    console.log(`Started on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
